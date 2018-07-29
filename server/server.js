ce9489740adb7ed5ed9b82666864cae85fed7996
Shashwat A <proshashwat@gmail.com>
2018-07-29 15:41:39 +0530

    add connct disconnect

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 const express=require('express');
 const path=require('path');
+const http=require('http');
+const socketIO=require('socket.io');
 const port=process.env.PORT||3000;
 
 const publicPath=path.join(__dirname,'../public');
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=>{
+  console.log('New user connected');
+});
+io.on('disconnect',(socket)=>{
+  console.log('New user disconnected');
+});
 
-app.listen(port);
+
+server.listen(port);
